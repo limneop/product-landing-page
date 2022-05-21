8157db5b7816faa6f6e639a4b1f8b2a4d70ebf5a
Lawrence Limneo Platon <limneop@gmail.com>
2022-05-21 23:55:44 +0800

    darkmodeformobile

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function myFunction() {
     element.classList.toggle("dark-mode");
  }
 
+;(() => {
+    const searchParams = new URLSearchParams(window.location.search)
+    const name = searchParams.get('email')
+    console.log(name)
+    const domName = document.querySelector('[data-name]')
+    console.log(domName)
+  
+    if(domName) {
+      domName.textContent = 'We will send you an email on ' + email
+    }
+})()
